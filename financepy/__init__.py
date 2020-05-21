a6be7ef9a3f1fff795929b5b186492742e996191
domokane <domokane@users.noreply.github.com>
2020-05-21 17:19:06 +0200

    Added import statements to __init__.py files for easier notebook usability

--- financepy/__init__.py
+++ financepy/__init__.py
@@ -1,3 +1,6 @@
-#from version import __version__
+# from version import __version__
+# print("Loading FinancePy Version:", __version__)
 
-#print("Loading FinancePy Version:", __version__)
+#__all__ = ['finutils', 'market', 'models', 'portfolio', 'products', 'risk']
+
+# from financepy.finutils import *
--- financepy/products/bonds/__init__.py
+++ financepy/products/bonds/__init__.py
@@ -1,1 +1,10 @@
+from .FinBond import *
+from .FinAnnuity import *
+from .FinBondConvertible import *
+from .FinBondEmbeddedOption import *
+from .FinBondFRN import *
+from .FinBondFuture import *
+from .FinBondMarket import *
+from .FinBondMortgage import *
+from .FinBondOption import *
 
