354261b0ad44eda531293d1f24d96dd87b710a33
Fergal <dominic@dominic-Inspiron-660>
2020-06-25 12:59:55 +0200

    Make `generateSobol` compatible with numba

--- financepy/finutils/FinSobol.py
+++ financepy/finutils/FinSobol.py
@@ -32,7 +32,17 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import numpy as np
+from functools import partial
 
+from numba import njit, objmode
+
+def bytesToIntArray(l, s):
+    arr = s.split(b' ')
+    npArr = np.array(arr, dtype='int64')
+    result = np.pad(npArr, (0, l - len(npArr)))
+    return result
+
+@njit
 def generateSobol(numPoints, dimension):
     """
     Sobol points generator based on graycode order.
@@ -44,19 +54,22 @@ def generateSobol(numPoints, dimension):
      Return:
          point (nparray): 2-dimensional array with row as the point and column as the dimension.
     """
-    # convert numPoints and dimension into int in case numPoints and dimension are float.
-    numPoints = int(numPoints)
-    dimension = int(dimension)
 
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "./sobolcoeff.csv")
-    soboldir = np.loadtxt(path, delimiter=',', dtype='str', skiprows=1, max_rows=dimension-1)
+    with objmode(sArr='int64[:]', aArr='int64[:]', m_i='int64[:,:]'):
+        dirname = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(dirname, "sobolcoeff.csv")
+
+        sArr = np.loadtxt(path, delimiter=',', dtype="int64", skiprows=1, max_rows=dimension-1, usecols=(1,))
+        aArr = np.loadtxt(path, delimiter=',', dtype="int64", skiprows=1, max_rows=dimension-1, usecols=(2,))
+        maxLen = sArr[-1]
+        m_i = np.loadtxt(path, delimiter=',', dtype='int64', skiprows=1, max_rows=dimension-1, usecols=(3,), converters={3: partial(bytesToIntArray, maxLen)})
+
     
     # ll = number of bits needed
     ll = int(np.ceil(np.log(numPoints)/np.log(2.0)))
 
     # c[i] = index from the right of the first zero bit of i
-    c = np.zeros(shape=[numPoints])
+    c = np.zeros(numPoints, dtype=np.int64)
     c[0] = 1
     for i in range(1, numPoints):
         c[i] = 1
@@ -64,41 +77,34 @@ def generateSobol(numPoints, dimension):
         while value & 1:
             value >>= 1
             c[i] += 1
-    c = c.astype(int)
 
     # points initialization
-    points = np.zeros(shape=[numPoints, dimension])
+    points = np.zeros((numPoints, dimension))
 
     # ----- Compute the first dimension -----
     # Compute direction numbers v[1] to v[L], scaled by 2**32
-    v = np.zeros(shape=[ll+1])
+    v = np.zeros(ll+1)
     for i in range(1, ll+1):
         v[i] = 1 << (32-i)
         v[i] = int(v[i])
 
     #  Evalulate x[0] to x[N-1], scaled by 2**32
-    x = np.zeros(shape=[numPoints])
+    x = np.zeros(numPoints)
     x[0] = 0
     for i in range(1, numPoints):
         x[i] = int(x[i-1]) ^ int(v[c[i-1]])
         points[i, 0] = x[i]/(2**32)
 
-    # Clean variables
-    del v
-    del x
-
     # ----- Compute the remaining dimensions -----
     for j in range(1, dimension):
-
         # read parameters from file
-        s = int(soboldir[j - 1, 1])
-        a = int(soboldir[j - 1, 2])
-        mm = soboldir[j - 1, 3]
-        mm = mm.split()
-        m = np.array([0]+mm).astype(int)
+        s = sArr[j-1]
+        a = aArr[j-1]
+        mm = m_i[j-1]
+        m = np.concatenate((np.zeros(1), mm))
 
         # Compute direction numbers V[1] to V[L], scaled by 2**32
-        v = np.zeros(shape=[ll+1])
+        v = np.zeros(ll+1)
         if ll <= s:
             for i in range(1, ll+1):
                 v[i] = int(m[i]) << (32-i)
@@ -113,14 +119,10 @@ def generateSobol(numPoints, dimension):
                     v[i] = int(v[i]) ^ (((int(a) >> int(s-1-k)) & 1) * int(v[i-k]))
 
         # Evalulate X[0] to X[N-1], scaled by pow(2,32)
-        x = np.zeros(shape=[numPoints])
+        x = np.zeros(numPoints)
         x[0] = 0
         for i in range(1, numPoints):
             x[i] = int(x[i-1]) ^ int(v[c[i-1]])
             points[i, j] = x[i]/(2**32)
-
-        del m
-        del v
-        del x
 
     return points
