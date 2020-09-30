3f055c5b11bd8f191985f07c2d873cc88c5ecba7
domokane <domokane@users.noreply.github.com>
2020-09-30 09:44:17 +0200

    Fixed LiborSwap

--- tests/TestImports.py
+++ tests/TestImports.py
@@ -2,16 +2,10 @@
 # Copyright (C) 2018, 2019, 2020 Dominic O'Kane
 ###############################################################################
 
-
-# from financepy.finutils.FinDate import FinDate # Works
-# from ..financepy import *  # fails
-
-
 from financepy.finutils import *
 from financepy.products.libor import *
 from financepy.products.bonds import *
 from financepy.market.curves import *
-# from financepy.products.bonds.FinBond import *
 
 
 def test_Imports():
@@ -24,21 +18,22 @@ def test_Imports():
 
         dcType = FinDayCountTypes.ACT_360
         fixedFreq = FinFrequencyTypes.SEMI_ANNUAL
-        swap1 = FinLiborSwap(settlementDate, FinDate(1,1,2008), 0.05, fixedFreq, dcType)
-        swap2 = FinLiborSwap(settlementDate, FinDate(1,1,2009), 0.05, fixedFreq, dcType)
-        swap3 = FinLiborSwap(settlementDate, FinDate(1,1,2010), 0.05, fixedFreq, dcType)
+        swapType = FinLiborSwapTypes.PAYER
+        swap1 = FinLiborSwap(settlementDate, FinDate(1,1,2008), swapType, 0.05, fixedFreq, dcType)
+        swap2 = FinLiborSwap(settlementDate, FinDate(1,1,2009), swapType, 0.05, fixedFreq, dcType)
+        swap3 = FinLiborSwap(settlementDate, FinDate(1,1,2010), swapType, 0.05, fixedFreq, dcType)
         swaps = [swap1, swap2, swap3]
         discountCurve = FinLiborCurve(settlementDate, [], [], swaps)
 
         print(discountCurve)
 
+    issueDate = FinDate(1, 1, 2000)
     maturityDate = FinDate(1, 1, 2010)
     coupon = 0.05
     frequencyType = FinFrequencyTypes.ANNUAL
     accrualType = FinDayCountTypes.ACT_ACT_ICMA
 
-    bond = FinBond(maturityDate, coupon, frequencyType, accrualType)
-
+    bond = FinBond(issueDate, maturityDate, coupon, frequencyType, accrualType)
     print(bond)
 
 ###############################################################################
