364f88cdd503ec0d2fbd53619e247128c90787f5
domokane <domokane@users.noreply.github.com>
2020-10-01 18:26:26 +0200

    Rolled to 0.182 and pushed to PyPi

--- financepy/__init__.py
+++ financepy/__init__.py
@@ -1,10 +1,10 @@
 cr = "\n"
 
 s = "####################################################################" + cr
-s += "# FINANCEPY BETA Version " + str('0.180') + " - This build:  24 Sep 2020 at 14:46 #" + cr
+s += "# FINANCEPY BETA Version " + str('0.182') + " - This build:  01 Oct 2020 at 18:23 #" + cr
 s += "#      This software is distributed FREE & WITHOUT ANY WARRANTY    #" + cr
 s += "# For info and disclaimer - https://github.com/domokane/FinancePy  #" + cr
-s += "#    For bug reports and comments - allmypythonprojects@gmail.com  #" + cr
+s += "#      Send any bug reports or comments to quant@financepy.com     #" + cr
 s += "####################################################################"
 s += cr
 
