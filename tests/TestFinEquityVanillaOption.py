771c950df8946e5ac22940c5dd2f1aa899682f98
Andrew Tan <andrew.tan@162digitalcapital.com>
2021-01-02 13:52:06 -0500

    Extending test cases for IV calculation.

--- tests/TestFinEquityVanillaOption.py
+++ tests/TestFinEquityVanillaOption.py
@@ -12,6 +12,7 @@ from financepy.products.equity.FinEquityVanillaOption import FinEquityVanillaOpt
 from financepy.market.curves.FinDiscountCurveFlat import FinDiscountCurveFlat
 from financepy.models.FinModelBlackScholes import FinModelBlackScholes
 from financepy.finutils.FinDate import FinDate
+from financepy.finutils.FinError import FinError
 
 from FinTestCases import FinTestCases, globalTestCaseMode
 testCases = FinTestCases(__file__, globalTestCaseMode)
@@ -174,6 +175,75 @@ def test_FinEquityVanillaOption():
 
 ###############################################################################
 
+    K = [10, 20, 50, 100, 150, 200, 250]
+    T = [0.001, 0.01, 0.1, 0.5, 1.0, 2.0, 5.0]
+    sigma = [0.01, 0.10, 0.50, 1.0]
+    optionTypes = [FinOptionTypes.EUROPEAN_CALL, FinOptionTypes.EUROPEAN_PUT]
+    stockPrice = 100
+    HOURS_PER_YEAR = 365.25 * 24
+    convergenceFailure = 0
+    assertionFailure = 0
+    noResult = 0
+    successful = 0
+    numberTests = 0
+    for t in T:
+        expDate = valueDate.addHours(t * HOURS_PER_YEAR)
+        for k in K:
+            for vol in sigma:
+                bs_model = FinModelBlackScholes(vol)
+                for type_ in optionTypes:
+                    option = FinEquityVanillaOption(
+                        expDate, k, type_)
+                    value = option.value(
+                        valueDate,
+                        stockPrice,
+                        discountCurve,
+                        dividendYield,
+                        bs_model)
+
+                    try:
+                        impliedVol = option.impliedVolatility(
+                            valueDate, stockPrice, discountCurve, dividendYield, value)
+                        assert abs(impliedVol - vol) < 0.10
+                    except FinError:
+                        noResult += 1
+                    except AssertionError:
+                        assertionFailure += 1
+                        print("-----------------")
+                        print(f"Did not converge to expected value: {round(impliedVol, 2)} vs {vol}")
+                        print(
+                            "INPUTS\n",
+                            "Type:", type_.name,
+                            "ttm:", t,
+                            "strike:", k,
+                            "Expected IV:", vol
+                        )
+                    except RuntimeError:
+                        import traceback
+                        traceback.print_exc()
+                        convergenceFailure += 1
+                        print(
+                            "INPUTS\n",
+                            "Type:", type_.name,
+                            "ttm:", t,
+                            "strike:", k,
+                            "Expected IV:", vol
+                        )
+                    except Exception:
+                        import traceback
+                        traceback.print_exc()
+                        noResult += 1
+                    else:
+                        successful += 1
+                    finally:
+                        numberTests += 1
+    print("\nSuccessful:", successful)
+    print("Convergence failure:", convergenceFailure)
+    print("Inaccurate result:", assertionFailure)
+    print("No result (price too low)", noResult)
+    print("TOTAL:", numberTests)
+
+###############################################################################
 
 test_FinEquityVanillaOption()
 testCases.compareTestCases()
