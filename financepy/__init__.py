801181dc2a8c782bb14c600ecf194f86405b6f5a
domokane <domokane@users.noreply.github.com>
2021-07-10 09:28:29 +0200

    Created DerivaGem BondOption example

--- financepy/__init__.py
+++ financepy/__init__.py
@@ -1,7 +1,8 @@
 cr = "\n"
 
 s = "####################################################################" + cr
-s += "# FINANCEPY BETA Version " + str('0.200') + " - This build:  06 Jul 2021 at 23:06 #" + cr
+s += "# FINANCEPY BETA Version " + str('0.200') + " - This build:  09 Jul 2021 at 15:21 #" + cr
+s += "# **** NEW PEP8 COMPLIANT VERSION -- PLEASE UPDATE YOUR CODE  **** #" + cr
 s += "#      This software is distributed FREE & WITHOUT ANY WARRANTY    #" + cr
 s += "# For info and disclaimer - https://github.com/domokane/FinancePy  #" + cr
 s += "#      Send any bug reports or comments to quant@financepy.com     #" + cr
