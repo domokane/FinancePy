a45bf0904f3d5f78ee66cc58505b426d76801f41
domokane <domokane@users.noreply.github.com>
2021-10-13 19:07:38 +0200

    Fixed forceobj bug

--- financepy/__init__.py
+++ financepy/__init__.py
@@ -1,7 +1,7 @@
 cr = "\n"
 
 s = "####################################################################" + cr
-s += "# FINANCEPY BETA Version " + str('0.203') + " - This build:  20 Sep 2021 at 19:02 #" + cr
+s += "# FINANCEPY BETA Version " + str('0.203') + " - This build:  13 Oct 2021 at 18:43 #" + cr
 s += "#      This software is distributed FREE & WITHOUT ANY WARRANTY    #" + cr
 s += "#  Report bugs as issues at https://github.com/domokane/FinancePy  #" + cr
 s += "####################################################################"
