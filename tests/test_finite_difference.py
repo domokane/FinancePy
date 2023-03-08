1bdccc7a173eddfa42c4c9a8451c355bfcaf53a4
idorrington92 <idorrington92@gmail.com>
2023-03-08 18:10:56 +0000

    Adding docstrings. Grouping decision tree comparison tests together

--- tests/test_finite_difference.py
+++ tests/test_finite_difference.py
@@ -13,6 +13,13 @@ from pytest import approx
 
 
 def test_black_scholes_finite_difference():
+    """
+    Compare the output of black_schole_finite_difference to kBlack::fdRunner from
+    https://github.com/domokane/CompFin/blob/main/Week%204/xladdin/Utility/kBlack.cpp
+
+    Results are identical to 3dp. The residual is due to differences interest and
+    dividend rates determined from the discount and dividend curves.
+    """
     s0 = 1
     r = 0.04
     dividend_yield = 0.07
@@ -102,6 +109,9 @@ def test_black_scholes_finite_difference():
 
 
 def test_european_call():
+    """
+    Check finite difference method gives similar result to binomial tree
+    """
     stock_price = 50.0
     risk_free_rate = 0.06
     dividend_yield = 0.00
@@ -141,6 +151,9 @@ def test_european_call():
 
 
 def test_european_put():
+    """
+    Check finite difference method gives similar result to binomial tree
+    """
     stock_price = 50.0
     risk_free_rate = 0.06
     dividend_yield = 0.00
@@ -179,66 +192,10 @@ def test_european_put():
     assert v == approx(value[0], abs=1e-1)
 
 
-def test_call_option():
-    expiry_date = Date(1, 7, 2015)
-    strike_price = 100.0
-    option_type = OptionTypes.EUROPEAN_CALL
-    call_option = EquityVanillaOption(
-        expiry_date, strike_price, option_type)
-
-    valuation_date = Date(1, 1, 2015)
-    stock_price = 100
-    volatility = 0.30
-    interest_rate = 0.05
-    dividend_yield = 0.01
-    model = BlackScholes(volatility)
-    discount_curve = DiscountCurveFlat(valuation_date, interest_rate)
-    dividend_curve = DiscountCurveFlat(valuation_date, dividend_yield)
-
-    # Call option
-    v0 = call_option.value(valuation_date, stock_price,
-                           discount_curve, dividend_curve, model)
-
-    _, v = black_scholes_finite_difference(stock_price=stock_price, sigma=volatility,
-                                           expiry_date=expiry_date, valuation_date=valuation_date,
-                                           strike_price=100.0, discount_curve=discount_curve,
-                                           dividend_curve=dividend_curve, digital=0,
-                                           option_type=option_type, smooth=0, theta=0.5, wind=0,
-                                           num_std=5, num_steps=50, num_samples=200, update=False, num_pr=1)
-    assert v == approx(v0, 1e-1)
-
-
-def test_put_option():
-    expiry_date = Date(1, 7, 2015)
-    strike_price = 100.0
-    option_type = OptionTypes.EUROPEAN_PUT
-    put_option = EquityVanillaOption(
-        expiry_date, strike_price, option_type)
-
-    valuation_date = Date(1, 1, 2015)
-    stock_price = 100
-    volatility = 0.30
-    interest_rate = 0.05
-    dividend_yield = 0.1
-    model = BlackScholes(volatility)
-    discount_curve = DiscountCurveFlat(valuation_date, interest_rate)
-    dividend_curve = DiscountCurveFlat(valuation_date, dividend_yield)
-
-    # Call option
-    v0 = put_option.value(valuation_date, stock_price,
-                          discount_curve, dividend_curve, model)
-
-    _, v = black_scholes_finite_difference(stock_price=stock_price, sigma=volatility,
-                                           expiry_date=expiry_date, valuation_date=valuation_date,
-                                           strike_price=100.0, discount_curve=discount_curve,
-                                           dividend_curve=dividend_curve, digital=0,
-                                           option_type=option_type, smooth=0, theta=0.5, wind=0,
-                                           num_std=5, num_steps=50, num_samples=200, update=False, num_pr=1)
-
-    assert v == approx(v0, 1e-1)
-
-
 def test_american_call():
+    """
+    Check finite difference method gives similar result to binomial tree
+    """
     stock_price = 50.0
     risk_free_rate = 0.06
     dividend_yield = 0.05
@@ -278,6 +235,9 @@ def test_american_call():
 
 
 def test_american_put():
+    """
+    Check finite difference method gives similar result to binomial tree
+    """
     stock_price = 50.0
     risk_free_rate = 0.06
     dividend_yield = 0.05
@@ -314,3 +274,68 @@ def test_american_put():
         exercise,
         params)  # price, delta, gamma, theta
     assert v == approx(value[0], abs=1e-1)
+
+def test_call_option():
+    """
+    Check finite difference method gives similar result to BlackScholes model
+    """
+    expiry_date = Date(1, 7, 2015)
+    strike_price = 100.0
+    option_type = OptionTypes.EUROPEAN_CALL
+    call_option = EquityVanillaOption(
+        expiry_date, strike_price, option_type)
+
+    valuation_date = Date(1, 1, 2015)
+    stock_price = 100
+    volatility = 0.30
+    interest_rate = 0.05
+    dividend_yield = 0.01
+    model = BlackScholes(volatility)
+    discount_curve = DiscountCurveFlat(valuation_date, interest_rate)
+    dividend_curve = DiscountCurveFlat(valuation_date, dividend_yield)
+
+    # Call option
+    v0 = call_option.value(valuation_date, stock_price,
+                           discount_curve, dividend_curve, model)
+
+    _, v = black_scholes_finite_difference(stock_price=stock_price, sigma=volatility,
+                                           expiry_date=expiry_date, valuation_date=valuation_date,
+                                           strike_price=100.0, discount_curve=discount_curve,
+                                           dividend_curve=dividend_curve, digital=0,
+                                           option_type=option_type, smooth=0, theta=0.5, wind=0,
+                                           num_std=5, num_steps=50, num_samples=200, update=False, num_pr=1)
+    assert v == approx(v0, 1e-1)
+
+
+def test_put_option():
+    """
+    Check finite difference method gives similar result to BlackScholes model
+    """
+    expiry_date = Date(1, 7, 2015)
+    strike_price = 100.0
+    option_type = OptionTypes.EUROPEAN_PUT
+    put_option = EquityVanillaOption(
+        expiry_date, strike_price, option_type)
+
+    valuation_date = Date(1, 1, 2015)
+    stock_price = 100
+    volatility = 0.30
+    interest_rate = 0.05
+    dividend_yield = 0.1
+    model = BlackScholes(volatility)
+    discount_curve = DiscountCurveFlat(valuation_date, interest_rate)
+    dividend_curve = DiscountCurveFlat(valuation_date, dividend_yield)
+
+    # Call option
+    v0 = put_option.value(valuation_date, stock_price,
+                          discount_curve, dividend_curve, model)
+
+    _, v = black_scholes_finite_difference(stock_price=stock_price, sigma=volatility,
+                                           expiry_date=expiry_date, valuation_date=valuation_date,
+                                           strike_price=100.0, discount_curve=discount_curve,
+                                           dividend_curve=dividend_curve, digital=0,
+                                           option_type=option_type, smooth=0, theta=0.5, wind=0,
+                                           num_std=5, num_steps=50, num_samples=200, update=False, num_pr=1)
+
+    assert v == approx(v0, 1e-1)
+
