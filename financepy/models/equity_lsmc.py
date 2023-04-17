a45dd310aeb48c65f4d3d283ee7b18e035289e75
idorrington92 <idorrington92@gmail.com>
2023-04-17 22:34:31 +0100

    Fixed up the lsmc algorithm and added some unit tests

--- financepy/models/equity_lsmc.py
+++ financepy/models/equity_lsmc.py
@@ -21,14 +21,15 @@ import matplotlib.pyplot as plt
 # Longstaff and Schwartz. Work is needed to add laguerre Polynomials and 
 # other interpolation methods.
 
-@njit(float64(float64, float64, float64, float64, int64, int64, float64, int64,
-                 float64, int64, int64), fastmath=True, cache=False)
+#@njit(float64(float64, float64, float64, float64, int64, int64, float64, int64,
+#                 float64, int64, int64, int64), fastmath=True, cache=False)
 def equity_lsmc(s0, r, q, v,
                 num_steps_per_year,
                 num_paths,
                 texp,
                 option_type_value,
                 k,
+                poly_degree,
                 use_sobol,
                 seed):
     
@@ -40,13 +41,11 @@ def equity_lsmc(s0, r, q, v,
     num_steps = int(num_steps_per_year * texp)
     num_times = num_steps + 1
 
+    dt = texp / num_times
     times = np.linspace(0, texp, num_times)
-    dt = texp / num_steps
     rootdt = np.sqrt(dt)
     
-    vsqrtdt = v * rootdt
-    mu = r - q - v*v/2.0
-    innov = v * rootdt * np.random.standard_normal((num_times, num_paths))
+    mu = r - q - 0.5 * v**2
 
     if num_paths % 2 == 1:
         num_paths = num_paths + 1
@@ -54,12 +53,12 @@ def equity_lsmc(s0, r, q, v,
     half_num_paths = int(num_paths/2.0)
 
     st = np.zeros((num_times, num_paths), 'd')  # stock price matrix
-    st[0, :] = s0
+    st[0] = s0
 
     for it in range(1, num_times):
         gp = np.random.standard_normal(half_num_paths)
         g = np.concatenate((gp, -gp))
-        st[it, :] = st[it-1, :] * np.exp(mu * dt + v * g * rootdt)
+        st[it] = st[it-1] * np.exp(mu * dt + v * g * rootdt)
 
     
     # ensure forward price is recovered exactly
@@ -76,35 +75,38 @@ def equity_lsmc(s0, r, q, v,
     else:
         raise FinError("Unknown option type.")
 
+    # Set final values for value_matrix and stopping matrix
     value_matrix = np.zeros((exercise_matrix.shape))
     value_matrix[-1] = exercise_matrix[-1]
+    stopping = np.zeros_like(value_matrix)
+    stopping[-1] = np.where(exercise_matrix[-1] > 0, 1, 0)
 
-    # number of basis functions
-    poly_degree = 5
-
-    for it in range(num_steps-1, 0, -1):
-
-        df = np.exp(-r * dt)
-
-        regression2 = fit_poly(st[it], 
-                               value_matrix[it+1] * df, 
-                               poly_degree)
-
-        cont_value = eval_polynomial(regression2, st[it])
-
-        if 1 == 0:
-            plt.figure()
-            plt.scatter(st[it], value_matrix[it+1])        
-            plt.title("it = " + str(it))
-            plt.scatter(st[it], cont_value)
-
-        value_matrix[it] = np.where(exercise_matrix[it] > cont_value, 
-                                    exercise_matrix[it], 
-                                    value_matrix[it+1] * df)
-
-    dt = times[1] - times[0]
     df = np.exp(-r * dt)
-    value = df * np.mean(value_matrix[1])
+    for it in range(num_times-2, 0, -1):
+        regression2 = np.polynomial.laguerre.lagfit(st[it], value_matrix[it + 1] * df, poly_degree)
+        cont_value = np.polynomial.laguerre.lagval(st[it], regression2)
+        #vander = np.vander(st[it], poly_degree+1)
+        #regression2 = np.linalg.lstsq(vander, value_matrix[it+1] * df)[0]
+        #cont_value = eval_polynomial(regression2, st[it])
+        cont_value[cont_value < 0] = 0
+
+        # Should we exercise at this timestep?
+        stopping[it] = np.where(exercise_matrix[it] > cont_value, 1, 0)
+
+        value_matrix[it] = np.where(exercise_matrix[it] > cont_value,
+                                    exercise_matrix[it], 
+                                    cont_value)
+
+    # for each path find the earliest stopping time
+    values = np.zeros(value_matrix.shape[1])
+    for i in range(value_matrix.shape[1]):
+        # first value in row of stopping matrix that is greater than zero
+        s = np.argmax(stopping.T[i])
+
+        # This is the value of the path discounted to present value
+        values[i] = value_matrix[s, i] * np.exp(-r * times[s])
+    value = np.mean(values)
+
     return value
 
 ###############################################################################
