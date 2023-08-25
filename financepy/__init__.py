f12d6d6ab2076b50c783beb8399164cc038395f2
domokane <domokane@users.noreply.github.com>
2023-08-25 16:28:53 +0200

    Version 0.31 released

--- financepy/__init__.py
+++ financepy/__init__.py
@@ -1,7 +1,7 @@
 cr = "\n"
 
 s = "####################################################################" + cr
-s += "# FINANCEPY BETA Version " + str('0.300') + " - This build:  24 Aug 2023 at 17:25 #" + cr
+s += "# FINANCEPY BETA Version " + str('0.310') + " - This build:  25 Aug 2023 at 16:17 #" + cr
 s += "#     This software is distributed FREE AND WITHOUT ANY WARRANTY   #" + cr
 s += "#  Report bugs as issues at https://github.com/domokane/FinancePy  #" + cr
 s += "####################################################################"
