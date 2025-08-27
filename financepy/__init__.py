bf005bbb66b332e8b3b194e371ac2de841d9fb17
Dominic O'Kane <domokane@users.noreply.github.com>
2025-08-28 00:16:09 +0200

    Removed startup banner

--- financepy/__init__.py
+++
@@ -1,8 +0,0 @@
-CR = "\n"
-S = "####################################################################" + CR
-S += "#    FINANCEPY Version " + str('1.1') + " - This build: 28 Aug 2025 at 00:09      #" + CR
-S += "#     This software is distributed FREE AND WITHOUT ANY WARRANTY   #" + CR
-S += "#  Report bugs as issues at https://github.com/domokane/FinancePy  #" + CR
-S += "####################################################################"
-S += CR
-print(S)
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ print(">>>" + version_number_str + "<<<")
 ########################################################################################
 cr = "\n"
 
-if True:
+if False:
     with open("financepy//__init__.template", "r") as file:
         filedata = file.read()
 
