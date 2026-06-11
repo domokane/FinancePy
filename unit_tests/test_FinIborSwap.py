26f863c53391eef844e454fcfeb86d496509542e
unknown <guoyx@ccfund.com>
2026-06-11 09:04:52 +0800

    Add IborSwap EOM schedule tests

--- unit_tests/test_FinIborSwap.py
+++ unit_tests/test_FinIborSwap.py
@@ -42,3 +42,53 @@ def test_ibor_swap_end_of_month_aligns_coupon_grid():
         ibor_swaps=swaps,
         interp_type=InterpTypes.FLAT_FWD_RATES,
     )
+
+
+def test_ibor_swap_end_of_month_handles_non_eom_effective_date():
+    swap = IborSwap(
+        effective_dt=Date(15, 5, 2023),
+        term_dt_or_tenor="1Y",
+        fixed_leg_type=SwapTypes.PAY,
+        fixed_cpn=0.01,
+        fixed_freq_type=FrequencyTypes.QUARTERLY,
+        fixed_dc_type=DayCountTypes.ACT_360,
+        float_dc_type=DayCountTypes.ACT_360,
+        bd_type=BusDayAdjustTypes.NONE,
+        end_of_month=True,
+    )
+
+    assert swap.fixed_leg.payment_dts == [
+        Date(31, 5, 2023),
+        Date(31, 8, 2023),
+        Date(30, 11, 2023),
+        Date(29, 2, 2024),
+        Date(15, 5, 2024),
+    ]
+
+
+def test_ibor_swap_end_of_month_applies_to_float_leg_schedule():
+    swap = IborSwap(
+        effective_dt=Date(31, 5, 2023),
+        term_dt_or_tenor="1Y",
+        fixed_leg_type=SwapTypes.PAY,
+        fixed_cpn=0.01,
+        fixed_freq_type=FrequencyTypes.ANNUAL,
+        fixed_dc_type=DayCountTypes.ACT_360,
+        float_freq_type=FrequencyTypes.QUARTERLY,
+        float_dc_type=DayCountTypes.ACT_360,
+        bd_type=BusDayAdjustTypes.NONE,
+        end_of_month=True,
+    )
+
+    assert swap.float_leg.payment_dts == [
+        Date(31, 8, 2023),
+        Date(30, 11, 2023),
+        Date(29, 2, 2024),
+        Date(31, 5, 2024),
+    ]
+    assert swap.float_leg.start_accrued_dts == [
+        Date(31, 5, 2023),
+        Date(31, 8, 2023),
+        Date(30, 11, 2023),
+        Date(29, 2, 2024),
+    ]
